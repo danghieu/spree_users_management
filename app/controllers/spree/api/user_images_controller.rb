48cf7598316d82d07b415693821c0a5810ad465e
Hieu Nguyen Dang <danghieu193@gmail.com>
2016-04-25 11:56:35 +0700

    a

--- app/controllers/spree/api/user_images_controller.rb
+++ app/controllers/spree/api/user_images_controller.rb
@@ -1,20 +1,19 @@
 module Spree
   module Api
-    class UserImagesController < Spree::Admin::ResourceController
+    class UserImagesController < Spree::Api::BaseController
       skip_before_filter :verify_authenticity_token
       before_action :load_data
-      create.before :set_viewable
-      update.before :set_viewable
 
       def model_class
         Dish::UserImage
       end
 
       def create 
+        @user_image = @user.images.build
         params[:user_image][:viewable_id]=@user.id
-        invoke_callbacks(:create, :before)
-        @object.attributes = permitted_resource_params
-        if @object.save
+        set_viewable
+        @user_image.attributes = permitted_resource_params
+        if @user_image.save
           @status = [ { "messages" => "success"}]
           render "spree/api/logger/log",status: 200
         else
@@ -25,8 +24,10 @@ module Spree
 
       def update
         params[:user_image][:viewable_id]=@user.id
-        invoke_callbacks(:update, :before)
-        if @object.update_attributes(permitted_resource_params)
+        @user_image =Dish::UserImage.find(params[:id])
+        set_viewable
+        @user_image.attributes = permitted_resource_params
+        if @user_image.save
           @status = [ { "messages" => "success"}]
           render "spree/api/logger/log",status: 200
         else
@@ -36,6 +37,9 @@ module Spree
       end
 
       private 
+        def permitted_resource_params
+          params[:user_image].present? ? params.require(:user_image).permit! : ActionController::Parameters.new
+        end
 
         def load_data
           @user = Spree::User.find(params[:user_id])
